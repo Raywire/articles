2c9ca21d25414e7f516835e2a94b1ed9a6008665
Raywire <ryanwire@outlook.com>
2019-07-14 21:54:17 +0300

    Initial Commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+
+class ArticleController extends Controller
+{
+    // Secure the resources
+    // public function __construct()
+    // {
+    //   $this->middleware('auth:api');
+    // }
+
+    //Secure but exempt index and show page
+    // public function __construct()
+    // {
+    //   $this->middleware('auth:api')->except(['index', 'show']);
+    // }
+
+    public function index()
+    {
+        return Article::all();
+    }
+
+    public function show(Article $article)
+    {
+        return $article;
+    }
+
+    public function store(Request $request)
+    {
+        $article = Article::create($request->all());
+
+        return response()->json($article, 201);
+    }
+
+    public function update(Request $request, Article $article)
+    {
+        $article->update($request->all());
+
+        return response()->json($article, 200);
+    }
+
+    public function delete(Article $article)
+    {
+        $article->delete();
+
+        return response()->json(null, 204);
+    }
+}
